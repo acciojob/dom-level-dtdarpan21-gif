89a8c730af665d6d763323629c74c241081b8caa
dtdarpan21-gif <dtdarpan21@gmail.com>
2026-09-25 19:48:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function findDOMLevel(){
+	let element = document.getElementById("level");
+	let level = 0;
+	while (element.parentElement){
+		level++;
+		element = element.parentElement;
+	}
+	alert("The level of the element is :" +level);
+}
+findDOMLevel();
